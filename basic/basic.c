2019543e54a877cc0fccdce4a6a7581da613b042
Avesh kumar prajapati <149478146+Avesh6754@users.noreply.github.com>
2023-11-09 15:55:04 +0530

    Add files via upload

---
+++ basic/basic.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+main()
+
+{
+	printf("Name = Avesh kumar prajapati\n");
+	printf("Age = 22\n");
+	printf("DOB = 05/10/2001\n");
+	printf("Mobile No. = 8090357405\n");
+	printf("Address = 108,Ramkrishna society near aspas gododara surat =395010\n");
+	printf("University = Sawrnim startup innovation university\n");
+	printf("Course = BCA");
+}
