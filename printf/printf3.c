e08b81451e08488357b1f3fa25a13a2e847eea31
Avesh kumar prajapati <149478146+Avesh6754@users.noreply.github.com>
2023-11-15 12:55:12 +0530

    Add files via upload

---
+++ printf/printf3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+main()
+
+{
+	printf(" - - - \n");
+	printf("|     |\n");
+	printf("N     |\n");
+	printf("A     |\n");
+	printf("M     |\n");
+	printf("E     |\n");
+	printf("|     |\n");
+	printf(" - - - \n");
+	
+}
