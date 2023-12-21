d68b74f2ac7dea5b605f1d493787cb1d4c27af99
Avesh kumar prajapati <149478146+Avesh6754@users.noreply.github.com>
2023-12-21 13:19:37 +0530

    Add files via upload

---
+++ looping with pattern2/S.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+
+{
+	int i,j;
+	
+	for(i=1;i<=7;i++)
+	{
+		for(j=1;j<=5;j++)
+		{
+			if(j==1 && i>1&&i<4 || (i==1 && j>1 )|| (i==4 && j>1&&j<5 ) || (j==5 && i>4&& i<7)||(i==7 && j>0&&j<5))
+			{
+				printf(" *");
+			}
+			else
+			{
+				printf("  ");
+			}
+		}
+			printf("\n");
+	}
+}
