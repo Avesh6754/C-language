14b99201f5940d1d9e3f110fdade2c90182b74c3
Avesh kumar prajapati <149478146+Avesh6754@users.noreply.github.com>
2024-01-10 15:39:36 +0530

    Add files via upload

---
+++ pointer2/task3/4.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void change(int *ptr)
+{
+	int n;
+	printf("enter the value of n :");
+	scanf("%d",&n);
+	
+	*ptr=*ptr+n;
+}
+main()
+{
+	int a;
+	printf("enter the value of a :");
+	scanf("%d",&a);
+	
+	printf("%d\n",a);
+	
+	int *ptr=&a;
+	
+	change(ptr);
+	printf("%d",a);
+	
+}
